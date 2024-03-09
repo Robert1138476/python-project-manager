1dffce581417d61caa1774cd8c8442a25de500fd
Robert <rtp432@gmail.com>
2024-03-09 01:30:53 -0500

    added edit functinality and fixed all bugs

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import subprocess
 
 print("Robert's python project organizer")
 
+if not os.path.exists("projects"):
+    os.makedirs("projects")
+
 projects = os.listdir("projects")
 print(f"Found {len(projects)} projects")
 
@@ -20,24 +23,31 @@ while True:
     while True:
         if command == "c":
             print(f"\nselect a template to create a new project")
+
             for i, template in enumerate(templates):
                 print(f"{i+1} - {template}")
+
+            selection = input("> ")
+            if selection == "b":
+                break
+
             try:
-                selection = input("> ")
-                if selection == "b":
-                    break
                 selection = int(selection) - 1
-            except:
+            except ValueError:
                 print("invalid selection")
+                break
+
             name = input("give your project a name: ")
             try:
-                shutil.copytree(f"{templates[selection]}", f"projects/{name}")
+                shutil.copytree(f"templates/{templates[selection]}", f"projects/{name}")
                 print("Project created successfully!")
             except FileExistsError:
                 print("Error: Project already exists.")
             except Exception as e:
                 print("An error occurred:", e)
-            print("project created")
+                time.sleep(5)
+                quit()
+
             print("opening project...")
             time.sleep(1)
             command = os.path.abspath(f"projects/{name}")
@@ -46,10 +56,116 @@ while True:
             quit()
 
         elif command == "o":
-            print("open project")
+            print(f"\nselect a project to open")
+
+            for i, project in enumerate(projects):
+                print(f"{i+1} - {project}")
+
+            selection = input("> ")
+            if selection == "b":
+                break
+            try:
+                selection = int(selection) - 1
+            except ValueError:
+                print("invalid selection")
+                break
+
+            command = os.path.abspath(f"projects/{projects[selection]}")
+            command = f'code "{command}"'
+            subprocess.run(command, shell=True, capture_output=False)
+            quit()
+
         elif command == "m":
-            print("manage projects")
+            print("select a project to manage")
+
+            for i, project in enumerate(projects):
+                print(f"{i+1} - {project}")
+
+            selection = input("> ")
+            if selection == "b":
+                break
+            try:
+                selection = int(selection) - 1
+            except ValueError:
+                print("invalid selection")
+                break
+
+            print(
+                f"\nselect a option to manage {projects[selection]}\nd - delete project\nr - rename project"
+            )
+
+            option = input("> ")
+            if option == "d":
+                shutil.rmtree(f"projects/{projects[selection]}")
+                print("project deleted")
+            elif option == "r":
+                name = input("give your project a new name: ")
+
+                shutil.move(f"projects/{projects[selection]}", f"projects/{name}")
+                print("project renamed")
+            elif option == "b":
+                break
+            else:
+                print("invalid selection")
+
+            time.sleep(2)
+            quit()
+
         elif command == "t":
-            print("manage templates")
+            print("select a template to manage or n to create a new one")
+
+            for i, template in enumerate(templates):
+                print(f"{i+1} - {template}")
+
+            selection = input("> ")
+            if selection == "b":
+                break
+            elif selection == "n":
+                name = input("give your template a name: ")
+
+                os.mkdir(f"templates/{name}")
+                print("template created")
+                print("opening template...")
+                time.sleep(1)
+
+                command = os.path.abspath(f"templates/{name}")
+                command = f'code "{command}"'
+                subprocess.run(command, shell=True, capture_output=False)
+                quit()
+            try:
+                selection = int(selection) - 1
+            except ValueError:
+                print("invalid selection")
+                break
+
+            print(
+                f"\nselect a option to manage {templates[selection]}\ne - edit template\nd - delete template\nr - rename template"
+            )
+
+            option = input("> ")
+            if option == "e":
+                print("opening template...")
+                time.sleep(1)
+
+                command = os.path.abspath(f"templates/{templates[selection]}")
+                command = f'code "{command}"'
+                subprocess.run(command, shell=True, capture_output=False)
+                quit()
+            elif option == "d":
+                shutil.rmtree(f"templates/{templates[selection]}")
+                print("template deleted")
+            elif option == "r":
+                name = input("give your template a new name: ")
+
+                shutil.move(f"templates/{templates[selection]}", f"templates/{name}")
+                print("template renamed")
+            elif option == "b":
+                break
+            else:
+                print("invalid selection")
+
+            time.sleep(2)
+            quit()
         else:
             print("invalid command")
+            break
